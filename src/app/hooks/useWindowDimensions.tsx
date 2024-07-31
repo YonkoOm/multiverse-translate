4a0b6004601b746b919c697a0ce53597f6129613
Om Moradia <moradiaom1@gmail.com>
2024-07-30 21:21:10 -0500

    check if window is undefined

--- src/app/hooks/useWindowDimensions.tsx
+++ src/app/hooks/useWindowDimensions.tsx
@@ -1,14 +1,17 @@
 import { useState, useEffect } from "react";
 
-function getWindowDimensions() {
+const getWindowDimensions = () => {
+  if (typeof window === "undefined") {
+    return { width: 0, height: 0 };
+  }
   const { innerWidth: width, innerHeight: height } = window;
   return {
     width,
     height,
   };
-}
+};
 
-export default function useWindowDimensions() {
+const useWindowDimensions = () => {
   const [windowDimensions, setWindowDimensions] = useState(
     getWindowDimensions(),
   );
@@ -23,4 +26,6 @@ export default function useWindowDimensions() {
   }, []);
 
   return windowDimensions;
-}
+};
+
+export default useWindowDimensions;
